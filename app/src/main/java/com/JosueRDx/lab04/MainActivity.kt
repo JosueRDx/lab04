ad04c9c89b61644f31a6a5413201a7cfe71b609c
JosueRDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-09-01 01:21:22 +0200

    feat: add RadioButtonExample component

--- app/src/main/java/com/JosueRDx/lab04/MainActivity.kt
+++ app/src/main/java/com/JosueRDx/lab04/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.JosueRDx.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab04Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    //función a probar
+                    MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        RadioButtonExample()
+    }
+
+}
+
+@Composable
+fun RadioButtonExample() {
+    var selected by remember { mutableStateOf(false) }
+
+    RadioButton(selected = selected, onClick = { selected = !selected })
+    Text(text = "RadioButton")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab04Theme {
-        Greeting("Android")
-    }
+fun FlowRowPreview() {
+    MainScreen()
 }
