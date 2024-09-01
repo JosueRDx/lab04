ba22305f5690ce45150f93e7618aff5d2ed6e6a7
JosueRDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-09-01 03:59:36 +0200

    style: update DividerControl

--- app/src/main/java/com/JosueRDx/lab04/MainActivity.kt
+++ app/src/main/java/com/JosueRDx/lab04/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.JosueRDx.lab04.ui.theme.Lab04Theme
@@ -68,7 +69,7 @@ fun CheckboxExample() {
 
 @Composable
 fun DividerControl() {
-    HorizontalDivider(thickness = 5.dp)
+    HorizontalDivider(thickness = 10.dp, color = Color.Green)
 }
 
 @Preview(showBackground = true)
